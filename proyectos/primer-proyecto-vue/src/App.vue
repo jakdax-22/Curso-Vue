964a610de8c9a65a64cb852d7ee4aef3dc860c91
EnriqueIranzo <enriqueiranzomartinez4@gmail.com>
2024-04-14 21:15:54 +0200

    14/04/2024 21:15:54,47

---
+++ proyectos/primer-proyecto-vue/src/App.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { ref,reactive,computed } from 'vue';
+  const taller = "Taller la tuerca ";
+  //ref ()
+  const servicio1 = ref("Básico")
+  const servicio2 = ref("Especial")
+  //actualizo el valor de servicio2
+  servicio2.value="Premium";
+
+  //propiedad computada
+  const evaluarPrecio = computed(()=>{
+    return (repuesto1.precio < 120) ? "Oferta !!!" : "Precio regular";
+  });
+
+  //reactive(), objetos y arrays solo normalmente
+  const repuesto1 = reactive({
+    id:1,
+    nombre:"filtro de aceite",
+    precio:100,
+    disponibilidad:true
+  });
+
+  //Actualizo repuesto1
+  repuesto1.disponibilidad = false;
+
+  //método evaluarDisponibilidad
+  const evaluarDisponibilidad = () =>{
+    return repuesto1.disponibilidad ? "✔" : "❌";
+  }
+</script>
+
+<template>
+  <h1>{{ taller }}</h1>
+  <h3> Servicios disponibles</h3>
+  <ul>
+    <li>{{ servicio1 }}</li>
+    <li>{{ servicio2 }}</li>
+  </ul>
+  <hr>
+  <h3>Repuestos</h3>
+  <section>
+    <p> {{ evaluarDisponibilidad() }} </p>
+    <p>{{ repuesto1.nombre }} ... {{ repuesto1.precio }} €</p>
+    <p> -- {{ evaluarPrecio }}</p>
+  </section>
+</template>
+
+<style scoped>
+  h1,p,h3,li{
+    font-family: monospace;
+  }
+  section {
+    display: flex;
+  }
+</style>
---
+++ proyectos/proyectoDirectivas/src/App.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { ref } from 'vue';
+  const producto = ref({
+    nombre:"Monitor Sony G5",
+    tamano:24,
+    precio:1000,
+    vendido:true
+  });
+
+  const esRojo = ref(true);
+
+  const estiloRojo ={
+    width:"100px",
+    height:"100px",
+    backgroundColor:"red"
+  }
+  const estiloVerde ={
+    width:"100px",
+    height:"100px",
+    backgroundColor:"green"
+  }
+  const titulo = ref("Los mejores monitores");
+</script>
+
+<template>
+  <section>
+    <p v-bind:class="{comprado: producto.vendido}"> {{ producto.nombre }} </p>
+  </section>
+  <section v-bind:style="esRojo ? estiloRojo : estiloVerde">
+    <p>Esta es una sección de estilo dinámico</p>
+  </section>
+  <section>
+    <h3> {{ titulo }} </h3>
+    <input type="text" placeholder="editame" v-model="titulo">
+  </section>
+</template>
+
+<style scoped>
+  .comprado{
+    text-decoration: line-through;
+    color: dimgray;
+  }
+</style>
